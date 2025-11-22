c0f1e35038295ebd5df8124abc9b5d34fc65c22f
Francis <franmartin.-@outlook.com>
2025-11-22 20:56:41 -0300

    Entrega 7 NUEVO

---
+++ ENTREGA 7/Entrega 7_NUEVO.sql
@@ -0,0 +1,259 @@
+
+-- 0) Contexto
+USE Com3641G01;
+GO
+
+-- 1) Master Key
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Com3641G01';
+GO
+
+-- 2) Certificado
+CREATE CERTIFICATE Cert_Consorcio
+WITH SUBJECT = 'Cifrado de columnas sensibles consorcios';
+GO
+
+-- 3) Clave simétrica AES-256
+CREATE SYMMETRIC KEY SK_Consorcio
+WITH ALGORITHM = AES_256
+ENCRYPTION BY CERTIFICATE Cert_Consorcio;
+GO
+
+-- 4) Backups de claves (guardar en un almacén seguro)
+BACKUP CERTIFICATE Cert_Consorcio
+TO FILE = 'C:\TEMP\Cert_Consorcio.cer'
+WITH PRIVATE KEY (
+    FILE = 'C:\TEMP\Cert_Consorcio.pvk',
+    ENCRYPTION BY PASSWORD = 'Com3641G01'
+);
+GO
+
+--------------------------------------
+
+USE Com3641G01
+GO
+
+-- Abrir clave para la migración
+OPEN SYMMETRIC KEY SK_Consorcio DECRYPTION BY CERTIFICATE Cert_Consorcio;
+GO
+
+-----------------------------
+-- PropietarioInquilino
+-----------------------------
+ALTER TABLE dbo.PropietarioInquilino
+ADD DNI_c VARBINARY(256),
+    nombre_c VARBINARY(256),
+    apellido_c VARBINARY(256),
+    email_c VARBINARY(256),
+    telefono_c VARBINARY(256),
+    CVU_CBU_c VARBINARY(256);
+GO
+
+UPDATE dbo.PropietarioInquilino
+SET DNI_c      = EncryptByKey(Key_GUID('SK_Consorcio'), CAST(DNI AS VARCHAR(20))),
+    nombre_c   = EncryptByKey(Key_GUID('SK_Consorcio'), nombre),
+    apellido_c = EncryptByKey(Key_GUID('SK_Consorcio'), apellido),
+    email_c    = EncryptByKey(Key_GUID('SK_Consorcio'), email),
+    telefono_c = EncryptByKey(Key_GUID('SK_Consorcio'), telefono),
+    CVU_CBU_c  = EncryptByKey(Key_GUID('SK_Consorcio'), CVU_CBU);
+GO
+
+-----------------------------
+-- Consorcios
+-----------------------------
+ALTER TABLE dbo.Consorcios
+ADD CBU_CVU_c VARBINARY(256);
+GO
+
+UPDATE dbo.Consorcios
+SET CBU_CVU_c = EncryptByKey(Key_GUID('SK_Consorcio'), CBU_CVU);
+GO
+
+-----------------------------
+-- Proveedores
+-----------------------------
+ALTER TABLE dbo.Proveedores
+ADD cuenta_c VARBINARY(256);
+GO
+
+UPDATE dbo.Proveedores
+SET cuenta_c = EncryptByKey(Key_GUID('SK_Consorcio'), cuenta);
+GO
+
+-----------------------------
+-- Pagos_importados
+-----------------------------
+ALTER TABLE dbo.Pagos_importados
+ADD cuenta_origen_c VARBINARY(256);
+GO
+
+UPDATE dbo.Pagos_importados
+SET cuenta_origen_c = EncryptByKey(Key_GUID('SK_Consorcio'), cuenta_origen);
+GO
+
+-- Cerrar clave
+CLOSE SYMMETRIC KEY SK_Consorcio;
+GO
+------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE dbo.SP_PropietarioInquilino_CleanupAndRename
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- 1. Eliminar constraint dependiente
+    IF EXISTS (
+        SELECT 1 FROM sys.objects 
+        WHERE name = 'UQ_PropietarioInquilino_DNI_CBU' AND type = 'UQ'
+    )
+    BEGIN
+        ALTER TABLE dbo.PropietarioInquilino
+        DROP CONSTRAINT UQ_PropietarioInquilino_DNI_CBU;
+    END
+
+    -- 2. Eliminar columnas en claro (si existen)
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'DNI') IS NOT NULL
+        ALTER TABLE dbo.PropietarioInquilino DROP COLUMN DNI;
+
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'nombre') IS NOT NULL
+        ALTER TABLE dbo.PropietarioInquilino DROP COLUMN nombre;
+
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'apellido') IS NOT NULL
+        ALTER TABLE dbo.PropietarioInquilino DROP COLUMN apellido;
+
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'email') IS NOT NULL
+        ALTER TABLE dbo.PropietarioInquilino DROP COLUMN email;
+
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'telefono') IS NOT NULL
+        ALTER TABLE dbo.PropietarioInquilino DROP COLUMN telefono;
+
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'CVU_CBU') IS NOT NULL
+        ALTER TABLE dbo.PropietarioInquilino DROP COLUMN CVU_CBU;
+
+    -- 3. Renombrar columnas cifradas al nombre original
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'DNI_c') IS NOT NULL
+        EXEC sp_rename 'dbo.PropietarioInquilino.DNI_c', 'DNI', 'COLUMN';
+
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'nombre_c') IS NOT NULL
+        EXEC sp_rename 'dbo.PropietarioInquilino.nombre_c', 'nombre', 'COLUMN';
+
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'apellido_c') IS NOT NULL
+        EXEC sp_rename 'dbo.PropietarioInquilino.apellido_c', 'apellido', 'COLUMN';
+
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'email_c') IS NOT NULL
+        EXEC sp_rename 'dbo.PropietarioInquilino.email_c', 'email', 'COLUMN';
+
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'telefono_c') IS NOT NULL
+        EXEC sp_rename 'dbo.PropietarioInquilino.telefono_c', 'telefono', 'COLUMN';
+
+    IF COL_LENGTH('dbo.PropietarioInquilino', 'CVU_CBU_c') IS NOT NULL
+        EXEC sp_rename 'dbo.PropietarioInquilino.CVU_CBU_c', 'CVU_CBU', 'COLUMN';
+END;
+GO
+
+EXEC dbo.SP_PropietarioInquilino_CleanupAndRename
+
+------------------------------------------------------------------
+
+SELECT*
+FROM dbo.PropietarioInquilino
+
+
+------------------------------------------------------------------
+
+SELECT *
+FROM 
+
+
+
+------------------------------------------------------------------
+
+
+CREATE OR ALTER PROCEDURE dbo.SP_Proveedores_CleanupAndRename
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- 1. Eliminar constraints dependientes (si hubiera alguno sobre cuenta)
+    DECLARE @constraint NVARCHAR(128);
+    SELECT TOP 1 @constraint = name
+    FROM sys.objects
+    WHERE type = 'UQ' AND name LIKE '%Proveedores%' AND name LIKE '%cuenta%';
+
+    IF @constraint IS NOT NULL
+    BEGIN
+        EXEC('ALTER TABLE dbo.Proveedores DROP CONSTRAINT ' + @constraint);
+    END
+
+    -- 2. Eliminar columna en claro
+    IF COL_LENGTH('dbo.Proveedores', 'cuenta') IS NOT NULL
+        ALTER TABLE dbo.Proveedores DROP COLUMN cuenta;
+
+    -- 3. Renombrar columna cifrada
+    IF COL_LENGTH('dbo.Proveedores', 'cuenta_c') IS NOT NULL
+        EXEC sp_rename 'dbo.Proveedores.cuenta_c', 'cuenta', 'COLUMN';
+END;
+GO
+
+EXEC dbo.SP_Proveedores_CleanupAndRename
+
+-------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE dbo.SP_Consorcios_CleanupAndRename
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- 1. Eliminar constraints dependientes (si hubiera alguno sobre CBU_CVU)
+    DECLARE @constraint NVARCHAR(128);
+    SELECT TOP 1 @constraint = name
+    FROM sys.objects
+    WHERE type = 'UQ' AND name LIKE '%Consorcios%' AND name LIKE '%CBU%';
+
+    IF @constraint IS NOT NULL
+    BEGIN
+        EXEC('ALTER TABLE dbo.Consorcios DROP CONSTRAINT ' + @constraint);
+    END
+
+    -- 2. Eliminar columna en claro
+    IF COL_LENGTH('dbo.Consorcios', 'CBU_CVU') IS NOT NULL
+        ALTER TABLE dbo.Consorcios DROP COLUMN CBU_CVU;
+
+    -- 3. Renombrar columna cifrada
+    IF COL_LENGTH('dbo.Consorcios', 'CBU_CVU_c') IS NOT NULL
+        EXEC sp_rename 'dbo.Consorcios.CBU_CVU_c', 'CBU_CVU', 'COLUMN';
+END;
+GO
+
+EXEC dbo.SP_Consorcios_CleanupAndRename
+
+---------------------------------------------
+
+CREATE OR ALTER PROCEDURE dbo.SP_PagosImportados_CleanupAndRename
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- 1. Eliminar constraints dependientes (si hubiera alguno sobre cuenta_origen)
+    DECLARE @constraint NVARCHAR(128);
+    SELECT TOP 1 @constraint = name
+    FROM sys.objects
+    WHERE type = 'UQ' AND name LIKE '%Pagos_importados%' AND name LIKE '%cuenta_origen%';
+
+    IF @constraint IS NOT NULL
+    BEGIN
+        EXEC('ALTER TABLE dbo.Pagos_importados DROP CONSTRAINT ' + @constraint);
+    END
+
+    -- 2. Eliminar columna en claro
+    IF COL_LENGTH('dbo.Pagos_importados', 'cuenta_origen') IS NOT NULL
+        ALTER TABLE dbo.Pagos_importados DROP COLUMN cuenta_origen;
+
+    -- 3. Renombrar columna cifrada
+    IF COL_LENGTH('dbo.Pagos_importados', 'cuenta_origen_c') IS NOT NULL
+        EXEC sp_rename 'dbo.Pagos_importados.cuenta_origen_c', 'cuenta_origen', 'COLUMN';
+END;
+GO
+
+EXEC dbo.SP_PagosImportados_CleanupAndRename
+
+-----------------------------------------------------------
